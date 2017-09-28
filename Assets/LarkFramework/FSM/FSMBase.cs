de2eb937c8d1abd753b7f7d8b6094b60e1f7649d
evesgf <evesgf@163.com>
2017-09-28 18:46:40 +0800

    添加状态机测试文件

---
+++ Assets/LarkFramework/FSM/FSMBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.LarkFramework.FSM
+{
+    public class FSMBase
+    {
+        private readonly string m_Name;
+
+        public FSMBase() : this(null)
+        {
+
+        }
+
+        public FSMBase(string name)
+        {
+            m_Name = name ?? string.Empty;
+        }
+    }
+}
---
+++ Assets/LarkFramework/FSM/FSMState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.LarkFramework.FSM
+{
+    public class FSMState
+    {
+        public virtual void OnInit()
+        {
+
+        }
+
+        public virtual void OnEnter()
+        {
+
+        }
+
+        public virtual void OnUpdate()
+        {
+
+        }
+
+        public virtual void OnLeave()
+        {
+
+        }
+
+        public virtual void OnDestroy()
+        {
+
+        }
+
+        public void ChageState()
+        {
+
+        }
+    }
+}
